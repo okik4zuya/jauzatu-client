d270a8593006068bf7581b1a26677f56973911f8
okik4zuya <okik4zuya@gmail.com>
2022-03-10 14:02:46 +0700

    add admin route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Switcher from "././pages/Switcher";
+import { Home, Admin, Switcher } from "./pages";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<Admin />} />
         <Route path='/:slug' element={<Switcher />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
