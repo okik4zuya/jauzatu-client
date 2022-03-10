b21723c93c5481a59d2c6d46f1b1340cb2b9a04e
okik4zuya <okik4zuya@gmail.com>
2022-03-10 15:52:04 +0700

    update

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function PageNotFound() {
-  return <div>Page not found</div>;
+export default function PageNotFound() {
+  return (
+    <div className='relative h-screen w-screen'>
+      <div className='absolute top-1/2 left-1/2 w-[400px] h-[400px] bg-gold transform -translate-x-1/2 -translate-y-1/2'>
+        <div className='text-center relative top-1/2 text-5xl font-montserrat font-bold text-white'>
+          Page not found
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default PageNotFound;
