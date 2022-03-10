b9aeb82fa6d74b11c5015b226b3b08c559e4d85e
okik4zuya <okik4zuya@gmail.com>
2022-03-10 14:09:35 +0700

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Admin />} />
+        <Route path='/' element={<Switcher />} />
         <Route path='/admin' element={<Admin />} />
         <Route path='/:slug' element={<Switcher />} />
       </Routes>
